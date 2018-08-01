c8cd4b2078f2d231cfd8768c9df3f0d2753b59b0
Edmond <edmondye502@gmail.com>
2018-08-01 14:00:56 -0700

    added budget screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import BudgetScreen from './screens/BudgetScreen';
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <BudgetScreen/>
     );
   }
 }
