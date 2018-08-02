a1c8c4e9844e0f64114e02018b35998af1e178f2
Edmond <edmondye502@gmail.com>
2018-08-02 14:31:50 -0700

    fixed issue with nested data

--- screens/BudgetScreen.js
+++ screens/BudgetScreen.js
@@ -6,15 +6,20 @@ import Pages from '../components/Pages';
 
 // temp data passed to page component
 const PAGE_DATA = [
-	{ date: '0718', desc: 'McDonalds', price: '9.99'},
-	{ date: '0818', desc: 'Chipotle', price: '12.69'},
-	{ date: '0918', desc: 'Gas', price: '37.00'},
+	{ date: '0718', data: [{desc: 'McDonalds', price: '9.99'}, {desc: 'KFC', price: '4.78'}]},
+	{ date: '0818', data: [{desc: 'Chipotle', price: '12.69'}]},
+	{ date: '0918', data: [{desc: 'Gas', price: '37.00'}, {desc: 'Amazon', price: '31.54'}]},
 ];
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 
 class BudgetScreen extends Component {
+
+	componentDidMount() {
+
+	}
 
 	getDateCode() {
 		now = new Date();
@@ -26,36 +31,33 @@ class BudgetScreen extends Component {
 		return month + year;
 	}
 
-	renderItem = ({item}) => {
-		<View 
-			style={styles.viewStyle}
-		>
-			<Text style={styles.textStyle}>
-				{item.desc}{"\n"}
-				{item.price}{"\n"}
-				{this.getDateCode()}{"\n"}
-			</Text>
+
+	renderBudget = ({item}) => (
+		<Text style={styles.textStyle}>
+			{item.desc}
+			{item.price}
+		</Text>
+	);
+
+	renderItem = ({item}) => (
+		<View style={styles.viewStyle}>
+			<Text style={styles.textStyle}>{item.date}{"\n"}</Text>
+			<FlatList
+				data={item.data}
+				renderItem={this.renderBudget}
+			  keyExtractor={(item, index) => index.toString()}
+			/>
 		</View>
-	}
+	);
 
 	render() {
 		return (
 			<FlatList
 				data={PAGE_DATA}
-				renderItem={ ({ item }) => (
-					<View 
-						style={styles.viewStyle}
-					>
-						<Text style={styles.textStyle}>
-							{item.desc}{"\n"}
-							{item.price}{"\n"}
-							{this.getDateCode()}{"\n"}
-						</Text>
-					</View>
-				)}
-			  keyExtractor={ item => item.date.toString() }
+				renderItem={this.renderItem}
+			  keyExtractor={item => item.date.toString()}
 			  horizontal
-			  pagingEnabled={true}
+			  pagingEnabled
 
 			/>
 		);
