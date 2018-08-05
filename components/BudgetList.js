f1b099792768f1c17dbb24b51cb20ee779e4680a
Edmond <edmondye502@gmail.com>
2018-08-05 12:06:54 -0700

    styled budget as list and add temp delete button

--- components/BudgetList.js
+++ components/BudgetList.js
@@ -1,50 +1,60 @@
 import React, { Component } from 'react';
 import { View, Text, FlatList, Dimensions } from 'react-native';
-import { Header, List, ListItem } from 'react-native-elements';
+import { Header, Card, List, ListItem } from 'react-native-elements';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
+// temp monthly budget
+const monthly_budget = 10;
 
 class BudgetList extends Component {
 
+	onDeleteBudgetPress = (id) => {
+		console.log('delete budget ' + id);
+	}
 
 	renderBudget = ({item}) => (
-		<Text style={styles.textStyle}>
-			{item.desc} {item.price}
-		</Text>
+		<ListItem
+			key={item.desc}
+			title={item.desc}
+      rightTitle={item.price}
+      rightIcon={{name: 'delete'}}
+      onPressRightIcon={() => this.onDeleteBudgetPress(item.desc)}
+		/>
 	);
 
 	renderItem = ({item}) => (
 		<View style={styles.viewStyle}>
 			<Header
-			  centerComponent={{ text: item.date, style: styles.headerStyle }}
+				centerComponent={{ text: item.date, style: styles.headerStyle }}
 			/>
-			<FlatList
-				data={item.data}
-				renderItem={this.renderBudget}
-			  keyExtractor={(item, index) => index.toString()}
-			  showsVerticalScrollIndicator={false}
-			/>
+				<FlatList
+					data={item.data}
+					renderItem={this.renderBudget}
+					keyExtractor={(item, index) => index.toString()}
+					showsVerticalScrollIndicator={false}
+				/>
+
 		</View>
 	);
 
 	getItemLayout = (data, index) => (
-    { length: SCREEN_WIDTH, offset: SCREEN_WIDTH * index, index }
-  );
+			{ length: SCREEN_WIDTH, offset: SCREEN_WIDTH * index, index }
+		);
 
 	render() {
 		return (
 			<FlatList
 				data={this.props.data}
 				renderItem={this.renderItem}
-			  keyExtractor={item => item.date.toString()}
-			  horizontal
-			  showsHorizontalScrollIndicator={false}
-			  pagingEnabled
-			  getItemLayout={this.getItemLayout}
-			  initialScrollIndex={1}
-			  ref={(ref) => {this.flatListRef = ref;}}
+				keyExtractor={item => item.date.toString()}
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				pagingEnabled
+				getItemLayout={this.getItemLayout}
+				initialScrollIndex={1}
+				ref={(ref) => {this.flatListRef = ref;}}
 			/>
 		);
 	}
@@ -53,14 +63,12 @@ class BudgetList extends Component {
 
 const styles = {
 	textStyle: {
-		fontSize: 30,
 		color: 'black',
 		textAlign: 'center',
 		marginBottom: 15
 	},
 	viewStyle: {
 		flex: 1,
-		justifyContent: 'center',
 		width: SCREEN_WIDTH,
 	},
 	headerStyle: {
