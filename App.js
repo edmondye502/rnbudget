b68baa984605b0677f503a227f2b320a9a5ca482
Edmond <edmondye502@gmail.com>
2018-08-05 12:50:47 -0700

    setup redux store

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
+import store from './store';
 
 import BudgetScreen from './screens/BudgetScreen';
 
 export default class App extends React.Component {
   render() {
     return (
-      <BudgetScreen style={styles.container} />
+    	<Provider store={store}>
+      	<BudgetScreen style={styles.container} />
+      </Provider>
     );
   }
 }
